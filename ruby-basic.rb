dcd3bad5225ccbf78e997641c3e99b88bcfc7c6a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-23 15:08:26 +0000

    add

--- ruby-basic.rb
+++ ruby-basic.rb
@@ -1,95 +1,220 @@
 # Debugging 出力
-print "hello world\n" # 'print' は文字列を出力しますが、改行は含まれません
-puts "hello world" # 'puts' は文字列を出力し、その後に改行が追加されます
-p "hello world" # 'p' はデバッグ用にオブジェクトを人間が読める形式で出力します
+print "hello world\n"
+puts "hello world"
+p "hello world"
 
 # 変数の使用
-msg = "hello world" # 変数「msg」に文字列を代入
-puts msg # 変数の内容を出力
+message = "hello world"
+puts message
 
-msg = "hello world again" # 変数「msg」に新しい文字列を代入
-puts msg # 更新された変数の内容を出力
+message = "hello world again"
+puts message
 
 # 定数の使用
-VERSION = 1.1 # 定数「VERSION」を定義。定数名は大文字で始める必要があります
-puts VERSION # 定数の値を出力
+VERSION = 1.1
+puts VERSION
 
 # データ型 数値とその演算
-p 4.8.class # Float クラスを出力
-p 10 + 3 # 加算
-p 10 * 3 # 乗算
-p 2.4 + 3 # FloatとIntegerの加算
-p 10 / 3 # 除算（整数同士の除算は商が整数で返る）
-p 10.0 / 3 # FloatとIntegerの除算（商が小数で返る）
-p Rational(2, 5) + Rational(3, 4) # 有理数の加算
+p 4.8.class
+p 10 + 3
+p 10 * 3
+p 2.4 + 3
+p 10 / 3
+p 10.0 / 3
+p Rational(2, 5) + Rational(3, 4)
 
 # 数値メソッド
-p 52.6.round # 四捨五入
-p 52.6.floor # 小さい方の整数に丸める
-p 52.6.ceil # 大きい方の整数に丸める
+p 52.6.round
+p 52.6.floor
+p 52.6.ceil
 
 # データ型 文字列
-puts "hello world" # 文字列の出力
-name = "taguchi"
-puts "hello #{name}" # 変数を含んだ式展開
+name = "world"
+puts "hello #{name}"
 
 # 配列の操作
-colors = ["red", "blue", "yellow"] # 配列の定義
-p colors[0] # 配列から要素を取得
-colors[0] = "pink" # 要素の書き換え
-colors.push("gold") # 要素の追加
-p colors # 配列の出力
+colors = ["red", "blue", "yellow"]
+colors[0] = "pink"
+colors.push("gold")
+p colors
 
 # データ型 ハッシュ
-scores = {taguchi: 200, fkoji: 400} # シンボルをキーにしたハッシュの定義
-p scores[:taguchi] # キーを指定して値を取得
-scores[:fkoji] = 600 # キーを指定して値を更新
-p scores[:fkoji] # 更新された値の取得
+scores = { taguchi: 200, fkoji: 400 }
+scores[:fkoji] = 600
+p scores[:fkoji]
 
 # データ型 変換
 x = 50
 y = "3"
-p x + y.to_i # 文字列を整数に変換して計算
-p x.to_s + y # 数値を文字列に変換して連結
+p x + y.to_i
+p x.to_s + y
 
 # 制御構造
 score = 85
-if score > 80
-    puts "great!"
-elsif score > 60
-    puts "good!"
+case score
+when 81..100
+  puts "great!"
+when 61..80
+  puts "good!"
 else
-    puts "so so ..."
+  puts "so so ..."
 end
 
-Signal = "red"
-case Signal
+signal = "red"
+case signal
 when "red"
-    puts "stop!"
+  puts "stop!"
 when "green", "blue"
-    puts "go!"
+  puts "go!"
 when "yellow"
-    puts "caution!"
+  puts "caution!"
 else
-    puts "wrong signal"
+  puts "wrong signal"
 end
 
 # ループ
 10.times do |i|
-    puts "#{i}: hello"
+  puts "#{i}: hello"
 end
 
 # イテレーション
-(15..20).each do |i|
-    p i
-end
+(15..20).each { |i| p i }
 
-colors = ["red", "blue"]
-colors.each do |color|
-    p color
-end
+colors.each { |color| p color }
 
-scores = {taguchi: 200, fkoji: 400}
 scores.each do |name, score|
-    puts "#{name}: #{score}"
+  puts "#{name}: #{score}"
+end
+
+10.times do |i|
+  break if i == 7
+  p i
+end
+
+10.times do |i|
+  next if i == 7
+  p i
+end
+
+# メソッド定義
+def say_hi(name = "tom")
+  "hi #{name}"
+end
+
+p say_hi("taguchi")
+
+# クラス定義
+class User
+  @@count = 0
+  VERSION = 1.1
+  attr_accessor :name
+
+  def initialize(name)
+    @@count += 1
+    @name = name
+  end
+
+  def say_hi
+    puts "hi #{@name}"
+  end
+
+  def self.info
+    puts "User Class, #{@@count} instances."
+  end
+end
+
+tom = User.new("tom")
+tom.name = "tom Jr."
+tom.say_hi
+
+bob = User.new("bob")
+sam = User.new("sam")
+
+User.info
+p User::VERSION
+
+# サブクラス定義
+class AdminUser < User
+  def say_hello
+    puts "Hello from #{@name}"
+  end
+
+  def say_hi
+    puts "Hello from admin"
+  end
+end
+
+admin_tom = AdminUser.new("tom")
+admin_tom.say_hi
+admin_tom.say_hello
+
+# アクセス制限
+class AccessUser
+  def say_hi
+    puts "hi"
+    say_private
+  end
+
+  private
+
+  def say_private
+    puts "private"
+  end
+end
+
+access_user = AccessUser.new
+access_user.say_hi
+
+# モジュール定義
+module Movie
+  VERSION = 1.1
+
+  def self.encode
+    puts "encoding..."
+  end
+
+  def self.export
+    puts "exporting..."
+  end
+end
+
+Movie.encode
+Movie.export
+
+p Movie::VERSION
+
+# モジュールのインクルード
+module Debug
+  def info
+    puts "#{self.class} debug info"
+  end
+end
+
+class Player
+  include Debug
+end
+
+class Monster
+  include Debug
+end
+
+Player.new.info
+Monster.new.info
+
+# エラーハンドリング
+class MyError < StandardError; end
+
+x = gets.to_i
+
+begin
+  raise MyError if x == 3
+  p 100 / x
+rescue MyError
+  puts "not 3!"
+rescue => exception
+  p exception.message
+  p exception.class
+  puts "stopped"
+ensure
+  puts "-- END --"
 end
